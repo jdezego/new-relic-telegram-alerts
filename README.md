b131b225675df6461b4ce4195e25bef6663c1529
James DeZego <james.dezego@gmail.com>
2023-01-05 16:17:57 -0500

    updates

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## This is a simple API Gateway that allows you to configure a Telegram webhook destination in a New Relic Workflow to send alerts to a Telegram channel or group.
 
-You must first create a Telegram Bot and add it to your channel/group as an admin: https://core.telegram.org/bots/tutorial
+You must first create a Telegram Bot and add it to your channel/group as an admin: https://core.telegram.org/bots/tutorial  
+
+- Create a Telegram channel or group
+- Add @BotFather to the channel/group and make it an admin
+- Follow instructions to create a new bot
 
 Get chat id for group or channel. Must already have messages in group/channel or this returns empty array:  
 https://api.telegram.org/bot[BOT_API_KEY]/getUpdates  
--- index.js
+++ index.js
@@ -8,11 +8,18 @@ app.use(express.json())
 
 //handle a post to the default route.
 //send New Relic alert JSON payload here and make sure the necessary Telegram fields are present, then build the final endpoint URL.
-//TODO: secure this! send api key/password in req
+//TODO: secure this! send api key/password(?) in req
 app.post("/", (req, res) => {
+    //these 2 must be created in the NR alert workflow payload via the UI and contain your actual api key and chat id
     const telegramBotAPIKey = req.body.telegramBotAPIKey
     const telegramChatID = req.body.telegramChatID
-    const telegramMessage = req.body.telegramMessage
+
+    //this will grab the entire JSON payload of the New Relic alert and send it as the Telegram message
+    const telegramMessage = JSON.stringify(req.body)
+
+    //or we could do something like this to grab individual fields of the alert payload
+    //const telegramMessage = req.body.totalIncidents
+
     const telegramEndpoint = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage?chat_id=${telegramChatID}&text=${telegramMessage}`
     res.end()
 
